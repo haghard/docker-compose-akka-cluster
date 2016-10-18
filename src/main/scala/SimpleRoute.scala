ad243add16345d3f73a6fb1bdb6dd2b4157ca944
Vadim Bondarev <haghard84@gmail.com>
2016-10-18 23:39:39 +0300

    add clustered events

--- src/main/scala/SimpleRoute.scala
+++ src/main/scala/SimpleRoute.scala
@@ -2,16 +2,15 @@ package main
 
 import akka.actor.ActorRef
 import akka.http.scaladsl.model._
-import akka.http.scaladsl.server._
+import akka.http.scaladsl.server.{Route, _}
 import akka.util.ByteString
 
-import akka.http.scaladsl.server.Route
-
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ExecutionContext, Future}
 
 class SimpleRoute(cluster: ActorRef, host: String)(implicit ex: ExecutionContext) extends Directives {
 
   import akka.pattern.ask
+
   import scala.concurrent.duration._
 
   implicit val _ = akka.util.Timeout(5 seconds)
@@ -19,10 +18,14 @@ class SimpleRoute(cluster: ActorRef, host: String)(implicit ex: ExecutionContext
   val route: Route =
     (get & path("members")) {
       complete {
-        (cluster ? 'Members).mapTo[String] { resp: String =>
-          HttpResponse(status = StatusCodes.OK, entity = HttpEntity(ContentTypes.`text/plain(UTF-8)`, ByteString(host)))
-        }
+        query()
+
       }
     }
 
+  def query: Future[HttpResponse] = {
+    (cluster ask 'Members).mapTo[String].map  { resp: String =>
+      HttpResponse(status = StatusCodes.OK, entity = HttpEntity(ContentTypes.`text/plain(UTF-8)`, ByteString(host)))
+    }
+  }
 }
