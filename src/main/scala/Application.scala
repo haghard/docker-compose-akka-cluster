6a12bae65d491eeeae6cb8a2b768f8b6d31c4087
Vadim Bondarev <haghard84@gmail.com>
2016-10-19 12:57:41 +0300

    a try to change port

--- src/main/scala/Application.scala
+++ src/main/scala/Application.scala
@@ -14,7 +14,7 @@ object Application extends App {
   val host = system.settings.config.getString("akka.remote.netty.tcp.hostname")
   val port = system.settings.config.getInt("akka.remote.netty.tcp.port")
 
-  println(s"HOST:PORT $host $port")
+  println(s"HOST:PORT - $host:$port")
 
   val cluster = system.actorOf(Props[ClusterMembershipSupport])
 
