e672f8be0298c3bb17b39b85c9e7f2a4b8020597
haghard <haghard84@gmail.com>
2019-10-15 12:56:00 +0300

    various improvements

--- src/main/scala/demo/DeviceShadowEntity.scala
+++ src/main/scala/demo/DeviceShadowEntity.scala
@@ -14,31 +14,39 @@ object DeviceShadowEntity {
   val entityKey: EntityTypeKey[DeviceCommand] =
     EntityTypeKey[DeviceCommand]("device")
 
+  //expect only akka.actor.typed.internal.PoisonPill
+  private def idle(log: akka.actor.typed.Logger): Behavior[DeviceCommand] = Behaviors.receiveSignal {
+    case (_, signal) ⇒
+      log.info(s"Signal ${signal.getClass.getName}")
+      Behaviors.stopped
+  }
+
   def apply(entityId: String, replicaName: String): Behavior[DeviceCommand] =
     Behaviors.setup { ctx ⇒
-      await(ctx.log, replicaName, entityId)
+      ctx.log.info("* * *  Start up entity * * *")
+      await(ctx.log, replicaName, entityId) orElse idle(ctx.log)
     }
 
-  def active(log: akka.actor.typed.Logger): Behavior[DeviceCommand] =
+  private def active(log: akka.actor.typed.Logger): Behavior[DeviceCommand] =
     Behaviors.receiveMessage {
       case PingDevice(id, _) ⇒
         log.info("ping entity {}", id)
         Behaviors.same
       case InitDevice(_) ⇒
+        //Ignore rerun wake up device because cluster membership has changed
         log.info("* * *  Wake up entity * * *")
         Behaviors.same
-      //Ignore rerun wake up device because cluster membership has changed
     }
 
-  def await(log: akka.actor.typed.Logger, replicaName: String, entityId: String): Behavior[DeviceCommand] =
+  private def await(log: akka.actor.typed.Logger, replicaName: String, entityId: String): Behavior[DeviceCommand] =
     Behaviors.receiveMessage {
       case InitDevice(_) ⇒
         log.warning("* * *  Wake up entity: {}  * * *", replicaName)
         //TODO: start replicator for the replicaName here !!!
-        active(log)
+        active(log) orElse idle(log)
       case PingDevice(id, _) ⇒
         log.warning("* * *  Wake up entity by ping: {}:{}  * * *", replicaName, id)
         //TODO: start replicator for the replicaName here !!!
-        active(log)
+        active(log) orElse idle(log)
     }
 }
